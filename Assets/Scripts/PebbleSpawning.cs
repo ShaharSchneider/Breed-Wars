796751de47992f53d431b6663a39868a0897a535
Yarin <Yarin.Mizrahi@s.afeka.ac.il>
2021-11-17 19:53:18 +0200

    Added a background and pebbles

---
+++ Assets/Scripts/PebbleSpawning.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PebbleSpawning : MonoBehaviour
+{
+    public GameObject[] pebbles = new GameObject[11];
+    public GameObject selector; //selected in the editor
+    private float width = (float)1.29;
+    private float height = (float)1.58;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < pebbles.Length; i++)
+        {
+            Vector3 currentVector;
+            if (i < 5)
+            {
+                currentVector = new Vector3((float)(-5 + (width * 1.3) * i), (float)-0.5, -1);
+            }
+            else
+            {
+                currentVector = new Vector3((float)(-5 + (width * 1.3) * (i - 5)), (float)-2.22, -1);
+            }
+            GameObject pebble = Instantiate(selector, currentVector, Quaternion.identity) as GameObject;
+            pebble.transform.localScale = new Vector3((float)width, (float)height);
+            pebbles[i] = pebble;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
